dfc16070d1ddccfb632742b47b0e32ff0c7f518a
Vici Shaweddy <vshaweddy@gmail.com>
2020-09-08 15:54:16 -0400

    expensive sequence finished

--- applications/expensive_seq/expensive_seq.py
+++ applications/expensive_seq/expensive_seq.py
@@ -1,8 +1,20 @@
-# Your code here
+import math
 
+lookup_table = {}
 
 def expensive_seq(x, y, z):
-    # Your code here
+    key = (x, y, z)
+
+    if key in lookup_table:
+        return lookup_table[key]
+
+    if x <= 0: 
+        result = y + z
+    if x >  0: 
+        result = expensive_seq(x-1,y+1,z) + expensive_seq(x-2,y+2,z*2) + expensive_seq(x-3,y+3,z*3)
+    
+    lookup_table[key] = result
+    return result
 
 
 
